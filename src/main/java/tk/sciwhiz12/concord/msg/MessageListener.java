da095e4131fb10ebc51c6bb15cd5bf9f16b004cc
SciWhiz12 <arnoldnunag12@gmail.com>
2021-03-28 17:12:06 +0800

    Queue messages from channel, and send out when on server thread
    This prevents any issues due to receiving a Discord message when not on the server thread.

--- src/main/java/tk/sciwhiz12/concord/msg/MessageListener.java
+++ src/main/java/tk/sciwhiz12/concord/msg/MessageListener.java
@@ -1,17 +1,24 @@
 package tk.sciwhiz12.concord.msg;
 
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.utils.MiscUtil;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ServerChatEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.server.ServerLifecycleHooks;
 import tk.sciwhiz12.concord.ChatBot;
 import tk.sciwhiz12.concord.ConcordConfig;
 
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class MessageListener extends ListenerAdapter {
+    // Using a concurrent queue because messages is received on a different thread from the main server thread.
+    private final Queue<MessageEntry> queuedMessages = new ConcurrentLinkedQueue<>();
     private final ChatBot bot;
 
     public MessageListener(ChatBot bot) {
@@ -28,13 +35,32 @@ public class MessageListener extends ListenerAdapter {
         if (event.getGuild().getIdLong() == MiscUtil.parseSnowflake(ConcordConfig.GUILD_ID) &&
             event.getChannel().getIdLong() == MiscUtil.parseSnowflake(ConcordConfig.CHANNEL_ID)) {
 
-            Messaging.sendToAllPlayers(bot.getServer(), event.getMember(),
-                event.getMessage().getContentDisplay());
+            queuedMessages.add(new MessageEntry(event));
+        }
+    }
+
+    @SubscribeEvent
+    void onServerTick(TickEvent.ServerTickEvent event) {
+        if (event.phase != TickEvent.Phase.END) return;
+
+        MessageEntry entry;
+        while ((entry = queuedMessages.poll()) != null) { // TODO: rate-limiting
+            Messaging.sendToAllPlayers(bot.getServer(), entry.member, entry.message.getContentDisplay());
         }
     }
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     void onServerChat(ServerChatEvent event) {
         Messaging.sendToChannel(bot.getDiscord(), event.getComponent().getString());
+    }
+
+    static class MessageEntry {
+        private final Member member;
+        private final Message message;
+
+        MessageEntry(GuildMessageReceivedEvent event) {
+            this.member = event.getMember();
+            this.message = event.getMessage();
+        }
     }
 }
