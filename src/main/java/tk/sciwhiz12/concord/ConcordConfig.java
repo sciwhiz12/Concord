ddc6817ff6dff194fa563dca74437a28638d014d
sciwhiz12 <arnoldnunag12@gmail.com>
2021-09-25 07:49:58 +0800

    Cleanup formatting

--- src/main/java/tk/sciwhiz12/concord/ConcordConfig.java
+++ src/main/java/tk/sciwhiz12/concord/ConcordConfig.java
@@ -58,13 +58,13 @@ public class ConcordConfig {
             builder.comment("Discord connection settings").push("discord");
 
             TOKEN = builder.comment("The token for the bot application.",
-                "If empty, the Discord integration will not be enabled.")
+                    "If empty, the Discord integration will not be enabled.")
                 .define("token", "");
             GUILD_ID = builder.comment("The snowflake ID of the guild where this bot belongs to.",
-                "If empty, the Discord integration will not be enabled.")
+                    "If empty, the Discord integration will not be enabled.")
                 .define("guild_id", "");
             CHANNEL_ID = builder.comment("The snowflake ID of the channel where this bot will post and receive messages.",
-                "If empty, the Discord integration will not be enabled.")
+                    "If empty, the Discord integration will not be enabled.")
                 .define("channel_id", "");
 
             builder.pop();
@@ -74,16 +74,16 @@ public class ConcordConfig {
             builder.comment("Message settings").push("messages");
 
             USE_CUSTOM_FONT = builder.comment("Use the custom `concord:icons` icons font (e.g owner crown) when possible.",
-                "If true, clients with the mod will use the custom icons font.",
-                "Set to false if you cannot ensure that all clients will have the mod installed.")
+                    "If true, clients with the mod will use the custom icons font.",
+                    "Set to false if you cannot ensure that all clients will have the mod installed.")
                 .define("use_custom_font", true);
 
             LAZY_TRANSLATIONS = builder.comment("Lazily translate the messages when possible.",
-                "This requires the clients have a resource pack with the messages, else they will render weirdly.",
-                "If false, all translation keys will be translated on the server.",
-                "If true, translation keys will translated on the server only if the client does not have the mod " +
-                    "installed.",
-                "Set to false if you cannot ensure that all clients will have the mod installed.")
+                    "This requires the clients have a resource pack with the messages, else they will render weirdly.",
+                    "If false, all translation keys will be translated on the server.",
+                    "If true, translation keys will translated on the server only if the client does not have the mod " +
+                        "installed.",
+                    "Set to false if you cannot ensure that all clients will have the mod installed.")
                 .define("lazy_translate", true);
 
             builder.pop();
@@ -91,12 +91,12 @@ public class ConcordConfig {
 
         {
             builder.comment("Mention settings",
-                "Settings for when messages from Concord that contain mentions should cause pings.",
-                "These are only bot-side settings; permissions on the bot user may prevent certain mentions from pinging.")
+                    "Settings for when messages from Concord that contain mentions should cause pings.",
+                    "These are only bot-side settings; permissions on the bot user may prevent certain mentions from pinging.")
                 .push("mentions");
 
             ALLOW_MENTIONS = builder.comment("Allow mentions to cause pings.",
-                "Disabling this setting effectively disables all other settings in this category.")
+                    "Disabling this setting effectively disables all other settings in this category.")
                 .define("allow_mentions", true);
 
             ALLOW_PUBLIC_MENTIONS = builder.comment("Allow @everyone and @here mentions to cause pings.")
@@ -113,49 +113,49 @@ public class ConcordConfig {
 
         {
             builder.comment("Game notification settings",
-                "Each setting controls a specific game to Discord notification message.")
+                    "Each setting controls a specific game to Discord notification message.")
                 .push("notify");
 
             SERVER_STOP = builder.comment("Complete startup of server",
-                "Translation key: message.concord.server.start")
+                    "Translation key: message.concord.server.start")
                 .define("server.start", true);
             SERVER_START = builder.comment("Stopping of server.",
-                "Translation key: message.concord.server.stop")
+                    "Translation key: message.concord.server.stop")
                 .define("server.stop", true);
 
             BOT_START = builder.comment("Enabling of Discord integration.",
-                "Translation key: message.concord.bot.start")
+                    "Translation key: message.concord.bot.start")
                 .define("bot.start", false);
             BOT_STOP = builder.comment("Disabling of Discord integration.",
-                "Translation key: message.concord.bot.stop")
+                    "Translation key: message.concord.bot.stop")
                 .define("bot.stop", false);
 
             PLAYER_JOIN = builder.comment("Player joining the game",
-                "Translation key: message.concord.player.join")
+                    "Translation key: message.concord.player.join")
                 .define("player.join", true);
             PLAYER_LEAVE = builder.comment("Player leaving the game",
-                "Translation key: message.concord.player.leave")
+                    "Translation key: message.concord.player.leave")
                 .define("player.leave", true);
             PLAYER_DEATH = builder.comment("Player death message",
-                "Translation key: message.concord.player.death")
+                    "Translation key: message.concord.player.death")
                 .define("player.death", true);
 
             PLAYER_ADV_GAMERULE = builder.comment("Whether to respect the `announceAdvancements` gamerule",
-                "If true, then the other advancement notifications settings only apply if the gamerule is true.",
-                "If false, the advancement notifications settings always apply.")
+                    "If true, then the other advancement notifications settings only apply if the gamerule is true.",
+                    "If false, the advancement notifications settings always apply.")
                 .define("player.adv.respect_gamerule", true);
             PLAYER_ADV_TASK = builder.comment("Player completed an normal advancement",
-                "Translation key: message.concord.player.advancement.task")
+                    "Translation key: message.concord.player.advancement.task")
                 .define("player.adv.task", true);
             PLAYER_ADV_CHALLENGE = builder.comment("Player completed a challenge advancement",
-                "Translation key: message.concord.player.advancement.challenge")
+                    "Translation key: message.concord.player.advancement.challenge")
                 .define("player.adv.challenge", true);
             PLAYER_ADV_GOAL = builder.comment("Player completed a goal advancement",
-                "Translation key: message.concord.player.advancement.goal")
+                    "Translation key: message.concord.player.advancement.goal")
                 .define("player.adv.goal", true);
 
             COMMAND_SAY = builder.comment("Message from /say command",
-                "Translation key: message.concord.command.say")
+                    "Translation key: message.concord.command.say")
                 .define("command.say", true);
 
             builder.pop();
