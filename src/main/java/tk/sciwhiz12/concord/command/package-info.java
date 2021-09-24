86ba46a37e56fa6aae48e0199db36ab89f931c35
sciwhiz12 <arnoldnunag12@gmail.com>
2021-09-25 07:51:46 +0800

    Add package-infos with non-null annotations

---
+++ src/main/java/tk/sciwhiz12/concord/command/package-info.java
@@ -0,0 +1,9 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+@FieldsAreNonnullByDefault
+package tk.sciwhiz12.concord.command;
+
+import net.minecraft.FieldsAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
