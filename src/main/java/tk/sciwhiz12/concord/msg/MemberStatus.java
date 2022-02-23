2fe5faabed8766a9abfc9ae7527705a24e67f1a1
sciwhiz12 <arnoldnunag12@gmail.com>
2022-02-23 22:38:34 +0800

    Update license headers
    The commit which added the license headers also causes them to be updated
    to the current year.

--- src/main/java/tk/sciwhiz12/concord/msg/MemberStatus.java
+++ src/main/java/tk/sciwhiz12/concord/msg/MemberStatus.java
@@ -1,5 +1,5 @@
 /*
- * Concord - Copyright (c) 2020-2021 SciWhiz12
+ * Concord - Copyright (c) 2020-2022 SciWhiz12
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
